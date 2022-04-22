9687541f7e1c9c7e0af788bed122a5f2c4d480e1
jpcampos <jpcampos@bancolombia.com.co>
2022-04-22 11:24:51 -0500

    production endpoint

--- src/infrastructure/httpClient.js
+++ src/infrastructure/httpClient.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = 'http://127.0.0.1:8080'
+const baseUrl = 'https://personal-page-back.herokuapp.com'
 
 const getSkills = async (skill)=> {
     let res = await axios.get(`${baseUrl}/skills`, { params: { prefix: skill }})
